b05be4a89dc0ffb2622b6d29fd46ac941428d34d
Dev_DucHieu <duchieu.dev@gmail.com>
2023-08-01 11:35:29 +0700

    [feat] meal detail

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -9,32 +9,7 @@
         />
     </div>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-        <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
-            <router-link to="/" >
-                <img 
-                :src="meal.strMealThumb" 
-                :alt="strMeal" 
-                class="rounded-t-xl w-full h-48 object-cover"
-                />
-            </router-link>
-            <div class="p-3">
-                <h3 class="font-bold">{{ meal.strMeal }}</h3>
-                <p class="mb-4">
-
-                </p>
-            <div class="felx items-center justify-between">
-                <a 
-                :href="meal.strYoutube" 
-                target="_blank" 
-                class="px-3 py-2 rounded border-2 text-white border-red-600 bg-red-500 hover:bg-red-600 hover:text-white transition-colors"
-                >
-                Youtube
-                </a>
-
-
-            </div>
-            </div>
-        </div>
+        <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal"/>
     </div>
 </template>
 
@@ -43,6 +18,8 @@ import { computed } from 'vue';
 import { onMounted,ref } from 'vue';
 import { useRoute } from 'vue-router';
 import store from '../store';
+import YoutubeButton from '../components/YoutubeButton.vue';
+import MealItem from '../components/MealItem.vue';
 
 const route = useRoute();
 const keyword = ref('');
